c1b3dbdbef1cb99e23db3ced40418b6c806656a8
fiqriardiansyah <fiqriardian92@gmail.com>
2024-12-25 11:14:50 +0700

    fix: changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,9 @@
 import { useEffect, useRef, useState } from 'react';
+import { Helmet } from 'react-helmet';
+import { Controller, Scene } from 'react-scrollmagic';
+import { Timeline, Tween } from 'react-gsap';
+
+
 import ArrowDownCircle from './assets/arrow-down-circle.svg';
 import ArrowUpRight from './assets/arrow-up-right.svg';
 import BgContactPng from './assets/bg-contact.png';
@@ -9,18 +14,34 @@ import Client1 from './assets/client-1.png';
 import Client2 from './assets/client-2.png';
 import Client3 from './assets/client-3.png';
 import Client4 from './assets/client-4.png';
+import Client5 from './assets/client-5.png';
+import Client6 from './assets/client-6.png';
+import Client7 from './assets/client-7.png';
+import Client8 from './assets/client-8.png';
+import Client9 from './assets/client-9.png';
+import Client10 from './assets/client-10.png';
+import Client11 from './assets/client-11.png';
+import Client12 from './assets/client-12.png';
+import Client13 from './assets/client-13.png';
+import Client14 from './assets/client-14.png';
 import FlowerSvg from './assets/flower.svg';
-import GearSvg from './assets/gear.svg';
-import HashSvg from './assets/hash.svg';
 import Head2Png from './assets/head-2.png';
 import HeadPng from './assets/head.png';
 import HopesyColorSvg from './assets/hopesy-color.svg';
 import Hopesy from './assets/hopesy.svg';
 import JumbotronPng from './assets/jumbotron.png';
-import RocketSvg from './assets/rocket.svg';
 import StarLinkSvg from './assets/star-link.svg';
 import StarSvg from './assets/star.svg';
 import TitleSvg from './assets/title.svg';
+import HashSvg from './assets/hash.svg';
+import RocketSvg from './assets/rocket.svg';
+import purseSvg from './assets/purse.svg';
+import calenderSvg from './assets/calendar.svg';
+import userSvg from './assets/user.svg';
+import sheetSvg from './assets/sheet.svg';
+import cursorSvg from './assets/cursor.svg';  
+import starPinSvg from './assets/star-pin.svg';
+import storeSvg from './assets/store.svg';
 
 const brands = [
   { img: Brand1, title: "Buzzing", desc: "Lorem ipsum dolor sit amet consectetur. Turpis id pulvinar" },
@@ -33,16 +54,28 @@ const clients = [
   { img: Client2, name: "pln" },
   { img: Client3, name: "astro" },
   { img: Client4, name: "adaro minerals" },
+  { img: Client5, name: "adaro minerals" },
+  { img: Client6, name: "adaro minerals" },
+  { img: Client7, name: "adaro minerals" },
+  { img: Client8, name: "adaro minerals" },
+  { img: Client9, name: "adaro minerals" },
+  { img: Client10, name: "adaro minerals" },
+  { img: Client11, name: "adaro minerals" },
+  { img: Client12, name: "adaro minerals" },
+  { img: Client13, name: "adaro minerals" },
+  { img: Client14, name: "adaro minerals" },
 ]
 
 const whatwedoitems = [
-  { img: RocketSvg, title: "Boost Campaign", desc: "Lorem ipsum dolor sit amet consectetur. Turpis id pulvinar " },
-  { img: HashSvg, title: "App Store & Play Store Optimization ", desc: "Lorem ipsum dolor sit amet consectetur. Turpis id pulvinar " },
-  { img: GearSvg, title: "Trending Hashtag Campaigns", desc: "Lorem ipsum dolor sit amet consectetur. Turpis id pulvinar " },
-  { img: GearSvg, title: "Survey Product", desc: "Lorem ipsum dolor sit amet consectetur. Turpis id pulvinar " },
-  { img: GearSvg, title: "User Generated Content", desc: "Lorem ipsum dolor sit amet consectetur. Turpis id pulvinar " },
-  { img: GearSvg, title: "Niche Influencer", desc: "Lorem ipsum dolor sit amet consectetur. Turpis id pulvinar " },
-  { img: GearSvg, title: "Voice & Visual Search Optimization", desc: "Lorem ipsum dolor sit amet consectetur. Turpis id pulvinar " },
+  { img: RocketSvg, title: "Boost Campaign", desc: "Enhance brand visibility through digital ads, influencers, buzzers, and trending hashtags on social media." },
+  { img: purseSvg, title: "App Store & Play Store Optimization ", desc: "Optimize apps to increase visibility, downloads, and reviews on app stores." },
+  { img: HashSvg, title: "Social Engagement Campaign ", desc: "Drive social media interactions through viral hashtags, user-generated content (UGC), and community campaigns." },
+  { img: calenderSvg, title: "Event & Community Activation", desc: "Activate physical events and communities to build brand loyalty and increase engagement through live experiences." },
+  { img: userSvg, title: "Influencer & Niche Campaign	", desc: "Leverage targeted influencers to reach the right audience and increase brand influence in relevant niches." },
+  { img: sheetSvg, title: "Market Insights & Surveys", desc: "Gather market insights and customer feedback through online and offline surveys." },
+  { img: cursorSvg, title: "Paid Ads Optimization", desc: "Optimize paid ads across platforms to increase reach, clicks, and conversions." },
+  { img: starPinSvg, title: "Reputation Management", desc: "Improve brand reputation online through positive reviews, ratings, and customer feedback on digital platforms." },
+  { img: storeSvg, title: "E-commerce Optimization", desc: "Optimize online shopping experiences to improve conversions and sales." },
 ]
 
 function App() {
@@ -74,7 +107,15 @@ function App() {
   }
  
   return (
-    <>
+    <Controller> 
+      <Helmet>
+      <meta property="og:title" content="Hopesy" />
+      <meta property="og:site_name" content="Hopesy" />
+      <meta property="og:url" content="" />
+      <meta property="og:description" content="A Platform-first community creative agency." />
+      <meta property="og:type" content="business.business" />
+      <meta property="og:image" content="https://ik.imagekit.io/p4ukigs1hrvx/Group%2050_E8Fv3tN6w.png?updatedAt=1733590113119" />
+      </Helmet>
       <div className={`w-[80vw] h-screen fixed top-0 left-0 flex items-center justify-center transform transition-all duration-500 z-50 translate-x-[-80vw] ${showMenu ? "!translate-x-0 delay-500" : ""}`}>
         <img src={JumbotronPng} className='absolute w-full h-full object-cover z-0 top-0 left-0' alt="Jumbotron" />
         <ul className='flex flex-col items-center gap-10 relative z-10'>
@@ -132,26 +173,58 @@ function App() {
           </button>
         </nav>
       </header>
-      <section style={{ filter: showMenu ? "grayscale(1)" : "" }} className='transition duration-300 w-full h-screen relative'>
-        <img src={JumbotronPng} className='absolute w-full h-full object-cover z-0' alt="Jumbotron" />
-        <div className="w-[80%] md:w-auto absolute z-20 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-          <div className="relative">
-            <img src={FlowerSvg} alt="" className="absolute z-10 right-0 top-0 rotate-animation" />
-            <img src={StarSvg} alt="" className="absolute star-animation top-1/2" />
-            <img src={TitleSvg} alt="" className="" />
+      <Scene duration="100%" triggerHook="onLeave" >
+        <Timeline  wrapper={<section style={{ filter: showMenu ? "grayscale(1)" : "" }} className='transition duration-300 w-full h-screen relative' />} >
+          <Tween 
+            position="0"
+            from={{ yPercent: -50, xPercent: -50 }}
+            to={{ yPercent: 0, xPercent: -50 }} >
+            <div className="w-[80%] md:w-auto absolute z-20 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+              <div className="relative">
+                <img src={FlowerSvg} alt="" className="absolute z-10 right-0 top-0 rotate-animation" />
+                <img src={StarSvg} alt="" className="absolute star-animation top-1/2" />
+                <img src={TitleSvg} alt="" className="" />
+              </div>
+            </div>
+          </Tween>
+          <img src={JumbotronPng} className='absolute w-full h-full object-cover z-0' alt="Jumbotron" />
+          <div className="absolute bottom-3 flex flex-col items-center gap-2 left-1/2 transform -translate-x-1/2">
+            <p className='text-white font-light text-center'>Scroll down to see more</p>
+            <img src={ArrowDownCircle} className='w-7 up-down-animation' alt="" />
           </div>
-        </div>
-        <div className="absolute bottom-3 flex flex-col items-center gap-2 left-1/2 transform -translate-x-1/2">
-          <p className='text-white font-light text-center'>Scroll down to see more</p>
-          <img src={ArrowDownCircle} className='w-7 up-down-animation' alt="" />
-        </div>
-      </section>
+        </Timeline>
+      </Scene>
       <section style={{ filter: showMenu ? "grayscale(1)" : "" }} className='transition duration-300 w-full bg-white min-h-screen' id='who-are-we'>
         <div className="flex h-full container-custom items-center">
           <div className="flex-1 relative min-h-screen hidden md:block">
-            <img src={BrandAwarePng} className="h-[200px] absolute top-1/2 transform -translate-y-1/2 right-[10%]" alt="" />
+          <div id="trigger-1" className=''></div>
+            <Scene triggerElement="#trigger1" offset={400} duration={300} >
+                {(progress) => (
+                    <Tween            
+                        to={{  y: '0', opacity: 1 }}     
+                        from={{ y: '200px', opacity: 0 }}  
+                        ease="Strong.easeOut"
+                        totalProgress={progress}
+                        paused
+                    >
+                        <img src={BrandAwarePng} className="h-[200px] absolute top-1/2 transform -translate-y-1/2 right-[10%]" alt="" />
+                    </Tween>    
+                )}
+            </Scene>
             <img src={HeadPng} className="h-[400px] z-10 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" alt="" />
-            <img src={BrandPerformPng} className="h-[300px] absolute bottom-0 transform z-20  left-[10%]" alt="" />
+            <Scene triggerElement="#trigger1" offset={500} duration={500} >
+                {(progress) => (
+                    <Tween            
+                        to={{  y: '0', opacity: 1 }}     
+                        from={{ y: '200px', opacity: 0 }}  
+                        ease="Strong.easeOut"
+                        totalProgress={progress}
+                        paused
+                    >
+                        <img src={BrandPerformPng} className="h-[300px] absolute bottom-0 transform z-20  left-[10%]" alt="" />
+                    </Tween>    
+                )}
+            </Scene>
           </div>
           <div className="flex-1 h-full flex flex-col gap-6 relative min-h-screen py-20">
             <img src={StarLinkSvg} className='h-[80%] pointer-events-none object-cover absolute top-1/2 -translate-x-1/2 -translate-y-1/2 left-1/2 transform' alt="" />
@@ -188,8 +261,8 @@ function App() {
           </div>
           <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-10">
             {whatwedoitems.map((element, i) => (
-              <div className="bg-dark rounded-lg p-3 md:p-7 flex flex-col gap-3" key={i}>
-                <img src={element.img} alt="" className='w-10' />
+              <div className="bg-dark rounded-lg p-3 md:p-7 flex flex-col gap-3 group hover:rotate-3 hover:scale-90 transition-all duration-150" key={i}>
+                <img src={element.img} alt="" className='w-10 group-hover:scale-150 duration-150 transition-all' />
                 <p className="m-0 text-primary-pink text-xl font-semibold">{element.title}</p>
                 <p className="m-0 text-white opacity-75 text-sm">{element.desc}</p>
               </div>
@@ -200,10 +273,10 @@ function App() {
       <section style={{ filter: showMenu ? "grayscale(1)" : "" }} className='transition duration-300 w-full py-14 bg-[#EFEFEF]' id='our-client'>
         <div className="container-custom flex flex-col items-center gap-10">
           <p className="m-0 text-primary-pink text-xl font-semibold">Our Client</p>
-          <p className="m-0 text-dark sm:text-2xl text-center font-semibold">We've partnered with many Brand to deliver insights and solutions to their problems with big data</p>
-          <div className="grid grid-cols-2 sm:grid-cols-4 gap-10">
+          <p className="m-0 text-dark sm:text-2xl lg:text-4xl text-center font-semibold">We've partnered with many Brand to deliver insights and solutions to their problems with big data</p>
+          <div className="flex flex-wrap gap-10 justify-center">
             {clients.map((client, i) => (
-              <img src={client.img} key={i} className="md:grayscale hover:grayscale-0 transition-all duration-300" alt={client.name} />
+              <img src={client.img} key={i} className=" transition-all w-auto object-contain duration-300" alt={client.name} />
             ))}
           </div>
         </div>
@@ -263,7 +336,7 @@ function App() {
             </div>
         </div>
       </section>
-    </>
+    </Controller>
   )
 }
 
